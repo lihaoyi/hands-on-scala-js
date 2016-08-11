5372c7532941c2f6901c43e2718025575b95b23a
Joost den Boer <jdboer@diversit.eu>
2016-08-11 21:05:22 +0200

    Format clock numbers to is always 2 positions
    Now n < 10 get prefixed with '0'.
    Whereas before a time would be displayed as '1:2:3', it is now shown as '01:02:03'.

--- examples/demos/src/main/scala/canvasapp/Clock.scala
+++ examples/demos/src/main/scala/canvasapp/Clock.scala
@@ -29,6 +29,11 @@ object Clock extends{
     renderer.textBaseline = "middle"
 
     /*code*/
+
+    // format the number with size 2 and optional '0' prefix
+    // so '1' becomes '01'
+    def format(n: Int) = f"$n%02d"
+    
     def render() = {
       val date = new js.Date()
       renderer.clearRect(
@@ -41,7 +46,7 @@ object Clock extends{
           date.getHours(),
           date.getMinutes(),
           date.getSeconds()
-        ).mkString(":"),
+        ).map(format).mkString(":"),
         canvas.width / 2,
         canvas.height / 2
       )
